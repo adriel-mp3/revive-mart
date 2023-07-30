ec96909e47ab2b2370c28e89f089a8b0cfc7e9b7
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-30 18:22:54 -0300

    :sparkles: feat(src): import header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Home from "./routes/Home/Home";
 import Jewelery from "./routes/Jewelery/Jewelery";
 import Woman from "./routes/Woman/Woman";
 import Men from "./routes/Men/Men";
+import Header from "./components/Header/Header";
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/jewelery" element={<Jewelery />} />
