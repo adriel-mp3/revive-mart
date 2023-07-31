c57956fdd6677c9d1c782dccdd60ccd80b6678df
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-31 14:11:23 -0300

    :sparkles: feat: import hero component to home

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import HeroContent from "../../components/HeroContent/HeroContent";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <main style={{ maxWidth: "1600px", margin: "0 auto", padding:'0 20px' }}>
+      <HeroContent />
+    </main>
+  );
+};
 
-export default Home
+export default Home;
