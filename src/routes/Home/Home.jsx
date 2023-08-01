86af323fa4bb8d57a526a9585a29960e8052bf11
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-07-31 21:16:56 -0300

    :sparkles: feat: import BestSellingProducts to home

--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import HeroContent from "../../components/HeroContent/HeroContent";
+import BestSellingProducts from "../../components/BestSellingProducts/BestSellingProducts";
+import AboutUs from "../../components/AboutUs/AboutUs";
 
 const Home = () => {
   return (
     <main style={{ maxWidth: "1600px", margin: "0 auto", padding:'0 20px' }}>
       <HeroContent />
+      <BestSellingProducts/>
     </main>
   );
 };
