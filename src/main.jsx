1840fb6cc10577be7bc10f08a4a7a154c30879d4
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-08-08 10:38:20 -0300

    :sparkles: feat: import theme provider

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { GlobalStyle } from "./styles/globalStyles";
 import { BrowserRouter } from "react-router-dom";
-
+import { Theme } from "./styles/Theme.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-      <GlobalStyle />
-    </BrowserRouter>
+    <Theme>
+      <BrowserRouter>
+        <App />
+        <GlobalStyle />
+      </BrowserRouter>
+    </Theme>
   </React.StrictMode>
 );
