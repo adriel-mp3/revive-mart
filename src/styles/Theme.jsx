de0ee573213f97992d4e75ea04f5c1c6774585a3
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-08-08 10:37:37 -0300

    :sparkles: feat: add theme from styled components

---
+++ src/styles/Theme.jsx
@@ -0,0 +1,27 @@
+import { ThemeProvider } from "styled-components";
+
+export const theme = {
+  colors: {
+    base: '#F8F8E6',
+    black: '#000000',
+    black100: '#1F1F1E',
+    black90: '#1B1B1B',
+    white: '#ffffff',
+    gray50: 'rgba(30, 30, 30, 0.50)',
+    gray100: '#5B5B57',
+    gray80: '#8E8E8E',
+    gray70: '#727270',
+    gray60: '#1E1E1E',
+  },
+  fontSizes: {
+    base: '1.125rem',
+    lg: '2rem',
+    xl: '4rem',
+  },
+};
+
+export const Theme = ({ children }) => (
+  <ThemeProvider theme={theme}>{children}</ThemeProvider>
+);
+
+
