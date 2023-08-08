c744d7a7e13679e747189898cc42f040c5eb11f9
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-08-08 10:13:25 -0300

    :recycle: refactor: remove unnecessary styled components

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { AboutContainer, AboutTitle, AboutText, AboutCards, AboutCard, CardImage, CardText, CardTitle, ImageWrapper } from './AboutUsStyles'
+import { AboutContainer, AboutCards, AboutCard, CardText, CardTitle, ImageWrapper } from './AboutUsStyles'
 import aboutCardsData from '../../utils/aboutCardsData';
 
 const AboutUs = () => {
   return (
     <AboutContainer>
-      <AboutTitle>About us</AboutTitle>
-      <AboutText>Embrace nature&apos;s allure with pre-loved finds. Order now!</AboutText>
+      <h2>About us</h2>
+      <p>Embrace nature&apos;s allure with pre-loved finds. Order now!</p>
       <AboutCards>
         {aboutCardsData.map((card) =>
           <AboutCard key={card.title}>
             <ImageWrapper>
-              <CardImage src={card.imgSrc} alt={card.imgAlt} />
+              <img src={card.imgSrc} alt={card.imgAlt} />
             </ImageWrapper>
             <CardTitle>{card.title}</CardTitle>
             <CardText>{card.text}</CardText>
