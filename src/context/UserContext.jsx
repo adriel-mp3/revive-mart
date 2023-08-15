2ad10d810cca5e79a66916487989070557d11522
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-08-15 11:34:19 -0300

    :sparkles: feat: add total cart

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export const UserContext = React.createContext();
 
 export const UserStorage = ({ children }) => {
-  const [cartData, setCartData] = React.useState([])
+  const [cartData, setCartData] = React.useState([]);
+  const totalCart = cartData.reduce((acc, item) => acc + (item.price * item.quantity), 0);
   return (
     <UserContext.Provider value={{ cartData, setCartData }}>
       {children}
