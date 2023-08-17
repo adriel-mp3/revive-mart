5102de05c1a7783fa830cd91c195827cb6b832c7
Adriel Santos <contatoadrielsantos@hotmail.com>
2023-08-17 13:26:55 -0300

    :sparkles: feat: add user context for cart

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -4,10 +4,54 @@ export const UserContext = React.createContext();
 
 export const UserStorage = ({ children }) => {
   const [cartData, setCartData] = React.useState([]);
-  const totalCart = cartData.reduce((acc, item) => acc + (item.price * item.quantity), 0);
-  const itemsOnCart = cartData.reduce((acc, item) => acc + item.quantity, 0);
+
+  const handleIncrement = (itemId) => {
+    const updatedCartItems = cartData.map(item => {
+      if (item.id === itemId) {
+        return {...item, quantity: item.quantity + 1};  
+      }
+      return item
+    })
+
+    setCartData(updatedCartItems);
+  }
+
+  const handleDecrement = (itemId) => {
+    const updatedCartItems = cartData.map(item => {
+      if (item.id === itemId && item.quantity > 1) {
+        return {...item, quantity: item.quantity - 1};  
+      }
+      return item
+    })
+
+    setCartData(updatedCartItems);
+  }
+
+  function addItemToCart(product) {
+    const existingProduct = cartData.find((item) => item.id === product.id);
+    
+    if (existingProduct) {
+      return setCartData((cartData) => cartData.map((item) =>
+        item.id === existingProduct.id ? { ...item, quantity: item.quantity + 1 } : item))
+    }
+    
+    setCartData((cartData) => [...cartData, {
+      id: product.id,
+      title: product.title,
+      price: product.price,
+      imgUrl: product.image,
+      category: product.category,
+      quantity: 1,
+    }])
+  }
+
+  const removeItemFromCart = (itemId) => {
+    const updatedCartItems = cartData.filter(item => item.id !== itemId);
+    setCartData(updatedCartItems);
+  };
+
   return (
-    <UserContext.Provider value={{ cartData, setCartData }}>
+    <UserContext.Provider value={{ cartData, setCartData, handleIncrement, handleDecrement, addItemToCart, removeItemFromCart}}>
       {children}
     </UserContext.Provider>
   )
